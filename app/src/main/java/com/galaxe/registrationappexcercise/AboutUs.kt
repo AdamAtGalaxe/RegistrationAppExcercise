fa4d834cbb4320ac0553a36734fb4a29ee0235df
AdamAtGalaxe <95375208+AdamAtGalaxe@users.noreply.github.com>
2021-12-06 22:15:40 -0500

    finished project 4 real 4real

--- app/src/main/java/com/galaxe/registrationappexcercise/AboutUs.kt
+++ app/src/main/java/com/galaxe/registrationappexcercise/AboutUs.kt
@@ -7,5 +7,6 @@ class AboutUs : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about_us)
+        supportActionBar?.hide()
     }
 }
--- app/src/main/java/com/galaxe/registrationappexcercise/loginPage.kt
+++ app/src/main/java/com/galaxe/registrationappexcercise/loginPage.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 
@@ -15,7 +14,7 @@ class loginPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
-
+        supportActionBar?.hide()
         userName = findViewById(R.id.user)
         password = findViewById(R.id.pass)
 
@@ -37,8 +36,9 @@ class loginPage : AppCompatActivity() {
 
     }
     fun back(v: View){
-        intent = Intent(this, MainActivity::class.java )
-        startActivity(intent)
+        finish()
+        //intent = Intent(this, MainActivity::class.java )
+        //startActivity(intent)
     }
     fun showAlert(v: View, msg: String){
         var alert = AlertDialog.Builder(this)
